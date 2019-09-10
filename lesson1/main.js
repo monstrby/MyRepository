8c8ead3b402a066506b2cf82e6338d39d57c5af1
Vitaly Belous <monstr.by@gmail.com>
2019-09-11 00:00:28 +0300

    Create add DOM elements functions

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,6 +1,1 @@
-var newText='Это наш новый текст, который меняется через функцию JS';
-function ChangeText() {
-  var oldText=document.getElementById('footerMsg');
-  oldText.textContent=newText;        
-}
-ChangeText();
+
